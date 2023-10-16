2deff0a03118a04d13e30bc27139df639c7509d2
nejcb-nhh <nejc.brancelj@student.nhh.no>
2023-10-16 22:08:11 +0200

    adding transform_volumes function

--- functions/data_transformations.R
+++ functions/data_transformations.R
@@ -26,3 +26,16 @@ to_iso8601 <- function(datetime, offset){
   return_date <- paste0(return_date,"Z") # Appending Z at the end of datetime
   return(return_date)
 }
+
+transform_volumes <- function(jsonOutput){
+  df <- jsonOutput %>%
+    pluck("trafficData", "volume", "byHour", "edges") %>%
+    map_df(function(x) {
+      data.frame(
+        from = as_datetime(x$node$from),
+        to = as_datetime(x$node$to),
+        volume = x$node$total$volumeNumbers$volume
+      )
+    })
+  return(df)
+}
